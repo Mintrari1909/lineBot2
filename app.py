10bc8c47537b11f54f0b2f2dee1ea0ad1dcc243f
Mintrari1909 <56124719+Mintrari1909@users.noreply.github.com>
2019-10-04 10:57:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,7 @@ def callback():
         sendText(user,'ไปตายที่ไหนก็ไป')
     else :
         sendText(user,'ว่าไงนะ')
-
-    return '',200
+        return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
